aeddb647f1a88f8d5ab5d8b41ffc3e73583161eb
MananGateshaniya <mananhiteshbhai898@gmail.com>
2025-07-17 12:06:04 +0530

    Add test for adding sweets

---
+++ Backend/test/SweetService.test.js
@@ -0,0 +1,26 @@
+const SweetService = require('../services/SweetService');
+const Sweet = require('../models/Sweet');
+
+describe('SweetService', () => {
+    let sweetService;
+
+    beforeEach(() => {
+        sweetService = new SweetService();
+    });
+
+    // Add Sweet Tests
+    test('should add a new sweet', () => {
+        const sweet = new Sweet(1, 'Chocolate Bar', 'chocolate', 2.5, 10);
+        sweetService.addSweet(sweet);
+        expect(sweetService.getAllSweets()).toContainEqual(sweet);
+    });
+
+    test('should not add sweet with duplicate id', () => {
+        const sweet1 = new Sweet(1, 'Chocolate Bar', 'chocolate', 2.5, 10);
+        sweetService.addSweet(sweet1);
+        const sweet2 = new Sweet(1, 'Candy', 'candy', 1.5, 20);
+        expect(() => sweetService.addSweet(sweet2)).toThrow('Sweet with this ID already exists');
+    });
+
+    
+});
